7fe475718ae48500d49b45a6649d60d62012fca5
msdhyt1208 <msdhyt1208@gmail.com>
2022-11-26 19:42:19 +0900

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+package HalloWorld;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hallo World");
+
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# HalloWorld
+# HalloWorld
+# HalloWorld
